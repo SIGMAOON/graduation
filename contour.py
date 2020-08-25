e16979bc10bde3c326adeb660fbbb28f56572b97
lamPolar <lovast7@gmail.com>
2020-08-25 21:53:01 +0900

    배경빼고 캐릭터 잡기 노력2

--- contour.py
+++ contour.py
@@ -1,11 +1,58 @@
 import cv2
 import numpy as np
+#가우시안 블러 + 캐니 디텍션 적용
 
+
+def Gray():
+    img = cv2.imread("jpg/overwatch275.jpg", cv2.IMREAD_COLOR)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #빨강색 outline 검출
+    lower = np.array([170, 30, 30], dtype="uint8")
+    upper = np.array([178, 255, 255], dtype="uint8")
+    mask = cv2.inRange(img_hsv, lower, upper)
+    img_color = cv2.bitwise_and(img_hsv, img_hsv, mask=mask)
+    # hsv to gray
+    img_rgb = cv2.cvtColor(img_color, cv2.COLOR_HSV2BGR)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    # 가우시안 블러, noise제거, threshold
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    img_dn = cv2.fastNlMeansDenoising(img_blur, 10, 10, 7, 21)
+    ret3, img_th = cv2.threshold(img_dn, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # contour, canny edge
+    canny = cv2.Canny(img_th, 70, 30)
+    #sobelx = cv2.Sobel(canny, cv2.CV_64F, 1, 0, ksize=3)
+    #sobely = cv2.Sobel(, cv2.CV_64F, 0, 1, ksize=3)
+    #laplacian = cv2.Laplacian(img, cv2.CV_8U)
+
+    contours, hierarchy = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        hull =cv2.convexHull(cnt)
+        img_hull = cv2.drawContours(canny, [hull], 0, (255,0,255),5)
+
+    contours, hierarchy = cv2.findContours(img_hull, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    max = 0
+    maxcnt = None
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if (max < area):
+            max = area
+            maxcnt = cnt
+    mask = np.zeros(img_hull.shape).astype(img_hull.dtype)
+    color = [255, 255, 255]
+    cv2.fillPoly(mask, [maxcnt], color)
+    img_hand = cv2.bitwise_and(img_hull, mask)
+    cv2.imshow('image', img_hand)
+
+    while True:
+        if cv2.waitKey(0) == 27:
+            cv2.destroyWindow('image')
+            break
+    return
+
+Gray()
 
 def Contour():
-    #cv2.namedWindow("noise removal")
     img = cv2.imread("jpg/overwatch275.jpg", cv2.IMREAD_COLOR)
-
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # 잡음 제거
     img_hsv = cv2.fastNlMeansDenoisingColored(img_hsv, None, 10, 10, 7, 21)
@@ -43,4 +90,4 @@ def Contour():
             break
     return
 
-Contour()
+#Contour()
